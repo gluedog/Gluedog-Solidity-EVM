cd278a4b4bcd45a2291ee01a1d7c9ead771a07f0
Glue Dog <32752940+gluedog@users.noreply.github.com>
2022-02-16 18:56:16 +0700

    Update gluedog.sol

--- Gluedog Token/gluedog.sol
+++ Gluedog Token/gluedog.sol
@@ -87,7 +87,7 @@ contract GluedogToken
      }
   
      /*  Allow _spender to withdraw from your account, multiple times, up to the _value amount. 
-          If this function is called again it overwrites the current allowance with _value. */
+         If this function is called again it overwrites the current allowance with _value. */
      function approve(address _spender, uint256 _amount) public returns (bool success) 
      {
         allowed[msg.sender][_spender] = _amount;
